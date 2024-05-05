684773227741ddff8b8daec5bc7445ccbd73baba
SaadyX <126882490+SaadyX@users.noreply.github.com>
2024-05-06 01:50:18 +0200

    last try

--- dataset.py
+++ dataset.py
@@ -4,11 +4,13 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
-# Définir votre classe de dataset personnalisé
+from sklearn.model_selection import train_test_split
+import math
 class DatasetPersonnalise(Dataset):
     def __init__(self, csv_file):
         self.data = pd.read_csv(csv_file, sep=';')  # Lire le fichier CSV dans un DataFrame
-
+        # Diviser les données en ensembles d'entraînement et de test
+        self.data_train, self.data_test = train_test_split(self.data, test_size=0.2, random_state=42)
     def __len__(self):
         return len(self.data)
 
@@ -20,6 +22,8 @@ class DatasetPersonnalise(Dataset):
         label = torch.tensor(label - 1).long()
         return sample, label
 
+    def set_train(self, train=True):
+        self.train = train
 # Chemin vers votre dataset CSV personnalisé
 fichier_csv = "new_data/labelled_dataset.csv"
 
@@ -29,16 +33,30 @@ dataset_personnalise = DatasetPersonnalise(fichier_csv)
 # Exemple d'utilisation du dataset personnalisé
 # Ici, nous allons parcourir le dataset en utilisant un DataLoader
 # et imprimer les premiers échantillons
-taille_lot = 32
+
+# Définir l'ensemble de données d'entraînement
+dataset_personnalise.set_train(True)
+taille_lot = 64
+chargeur_donnees_train = DataLoader(dataset=dataset_personnalise, batch_size=taille_lot, shuffle=True)
+dataset_personnalise.set_train(False)
+# Exemple d'utilisation du dataset personnalisé pour les données de test
+chargeur_donnees_test = DataLoader(dataset=dataset_personnalise, batch_size=taille_lot, shuffle=True)
+
 chargeur_donnees = DataLoader(dataset=dataset_personnalise, batch_size=taille_lot, shuffle=True)
 
-# for batch_idx, (samples, labels) in enumerate(chargeur_donnees):
-#    print("Batch:", batch_idx)
-#    print("Samples (features):", samples)
-#    print("Labels (targets):", labels)
- #   print("Batch size:", len(labels))
-  #  print()
+# Calculer le nombre d'itérations pour l'ensemble d'entraînement
+nb_iterations_train = math.ceil(len(dataset_personnalise.data_train) / taille_lot)
+# Calculer le nombre d'itérations pour l'ensemble de test
+nb_iterations_test = math.ceil(len(dataset_personnalise.data_test) / taille_lot)
 
+for batch_idx, (samples, labels) in enumerate(chargeur_donnees_test):
+    print("Batch:", batch_idx)
+    print("Samples (features):", samples)
+    print("Labels (targets):", labels)
+    print("Batch size:", len(labels))
+    print()
+    if batch_idx + 1 == nb_iterations_test:
+        break
 class MonCNN(nn.Module):
     def __init__(self):
         super(MonCNN, self).__init__()
@@ -46,15 +64,15 @@ class MonCNN(nn.Module):
         self.conv2 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1).double()
         self.conv3 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1).double()
         self.pool = nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
-        self.fc1 = nn.Linear(128 * 9, 128).double()  # Ajuster la taille en fonction de la sortie de la dernière couche de convolution
+        self.fc1 = nn.Linear(128 * 9, 128).double()
         self.fc2 = nn.Linear(128, 10).double()  # 10 classes pour la classification des activités
 
     def forward(self, x):
-        x = x.unsqueeze(1)  # Ajouter une dimension pour le canal (batch_size, 1, sequence_length)
+        x = x.unsqueeze(1)
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
-        x = x.view(-1, 128 * 9)  # Ajuster la taille en fonction de la sortie de la dernière couche de convolution
+        x = x.view(-1, 128 * 9)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
@@ -64,34 +82,61 @@ class SimpleCNN(nn.Module):
         super(SimpleCNN, self).__init__()
         self.conv1 = nn.Conv1d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1).double()
         self.pool = nn.MaxPool1d(kernel_size=2, stride=2, padding=0)
-        self.fc1 = nn.Linear(32 * 64, 128).double()  # Calculé pour correspondre à la taille de l'entrée
+        self.fc1 = nn.Linear(32 * 64, 128).double()  
         self.fc2 = nn.Linear(128, 11).double()  # 11 classes pour la classification
 
     def forward(self, x):
-        x = x.unsqueeze(1)  # Ajouter une dimension pour le canal (batch_size, 1, sequence_length)
+        x = x.unsqueeze(1)
         x = self.pool(F.relu(self.conv1(x)))
-        x = x.view(-1, 39936)  # Ajuster la taille en fonction de la sortie de la dernière couche de convolution
+        x = x.view(-1, 39936)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+
+
 # Créer une instance de votre modèle CNN
-model = SimpleCNN()
+model1 = SimpleCNN()
+model = MonCNN()
 
 # Définir la fonction de perte et l'optimiseur
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
-num_epochs = 10
+num_epochs = 100
+
+
+model.train()
 for epoch in range(num_epochs):
-    for samples, labels in chargeur_donnees:
-        outputs = model(samples)
-        loss = criterion(outputs, labels)
-
-        # Rétropropagation et mise à jour des poids
+    running_loss = 0.0
+    for i, data in enumerate(chargeur_donnees_train, 0):
+        inputs, labels = data
         optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
+        running_loss += loss.item()
 
-    # Imprimer la perte à la fin de chaque époque
-    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+    # Afficher la perte moyenne pour l'ensemble de l'époque
+    print('[%d] loss: %.3f' % (epoch + 1, running_loss / len(chargeur_donnees_train)))
+    running_loss = 0.0
+
+print('Finished Training')
+
+# Test du modèle
+correct = 0
+total = 0
+with torch.no_grad():
+    model.eval()
+    for data in chargeur_donnees_test:
+        inputs, labels = data
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the test datas: %d %%' % (
+    100 * correct / total))
+
+
 
